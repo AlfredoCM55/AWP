68a1945c7ec5beb14abbe91f8ef76b83da6d4391
AlfredoCM55 <joy.porta@gmail.com>
2022-10-13 14:28:44 -0500

    actualizado

---
+++ sw.js
@@ -0,0 +1,18 @@
+//asignar nombre y version de cache
+const CACHE_NAME = 'v1_cache_awp';
+
+//configurar los archivos del cache
+
+var urlToCache = [
+    './',
+    './css/style.css',
+    './images/convers.jpg',
+    './images/facebook.jpg',
+    './images/golf.jpg',
+    './images/instagram.jpg',
+    './images/playera.jpg',
+    './images/twiter.jpg',
+    './images/wallaper.jpg',
+    './images/wheel.jpg',
+    
+];
