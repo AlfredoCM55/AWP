f80072ebdfb5aa73c44a249d8d91561d12df6615
AlfredoCM55 <joy.porta@gmail.com>
2022-12-05 18:05:50 -0600

    Finalizado

--- sw.js
+++ sw.js
@@ -1,18 +1,46 @@
-//asignar nombre y version de cache
-const CACHE_NAME = 'v1_cache_awp';
+self.addEventListener('install', e=>{
+    caches.open('cache-v1')
+    .then(cache =>{
+        cache.addAll([
+            './',
+            'css/style.css',
+            'images/convers.jpg',
+            'images/facebook.png',
+            'images/golf.png',
+            'images/instagram.png',
+            'images/playera.jpg',
+            'images/wallaper.jpg',
+            'images/wheel.jpg',
+            'images/twiter.png',
+            'images/LEFLEUR.mp4',
+            'images/TYLER.mp4',
+            'images/CREATOR.mp4',
+            'images/twiter.png',
+            'main.js'
+        ])
+    });
+    e.waitUntil(cacheProm);
+});
 
-//configurar los archivos del cache
+self.addEventListener('fetch', e =>{
+    //cache with network fallback
+    const respuesta = caches.match( e.request )
+        .then ( res => {
+            if ( res ) return res;
+            //no existe el archivo
+            //tengo que ir a la web
+            console.log('No existe', e.request.url);
+            return fetch( e.request ).then ( newResp => {
+                caches.open('cache-v1')
+                    .then( cache => {
+                        cache.put( e.request, newResp);
+                    }
 
-var urlToCache = [
-    './',
-    './css/style.css',
-    './images/convers.jpg',
-    './images/facebook.jpg',
-    './images/golf.jpg',
-    './images/instagram.jpg',
-    './images/playera.jpg',
-    './images/twiter.jpg',
-    './images/wallaper.jpg',
-    './images/wheel.jpg',
-    
-];
+                    )
+                return newResp.clone;
+            });
+        });
+        e.respondWith(respuesta);
+    //only cache
+    //e.respondWith( caches.match(e.request));
+});
